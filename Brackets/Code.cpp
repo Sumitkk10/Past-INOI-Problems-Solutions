b7ef2bd7729a6b93892bf6cc2b4d41178fa2ef64
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-03-30 12:56:23 +0530

    Update Code.cpp

--- Brackets/Code.cpp
+++ Brackets/Code.cpp
@@ -1,49 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long int ll;
-#define tc ll test;cin >> test;while(test--)
-#define vi vector<ll>
-#define pb push_back
-#define mp make_pair
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long int
 #define INF 0x3f3f3f3f3f
 #define MOD 1000000007
-#define ff first
-#define ss second
-#define in >>
-#define out <<
-#define space << " " <<
-#define spacef << " "
-#define fo(i,a,b) for(ll i = a; i <= b; i++)
-#define nextline out "\n"
-#define print(x) for(auto i : x ) cout out i spacef;
-#define mmax(x,i) x = max(x,i)
-#define mmin(x,i) x = min(x,i)
 
 int main() {
 
+    fast;
+
     ll n,k;
-    cin in n in k;
+    cin >> n >> k;
 
-    vi value(n+5);
-    vi bracket(n+5);
+    vector<ll> value(n+5);
+    vector<ll> bracket(n+5);
+    
+    for(ll i = 1; i <= n; i++) cin >> value[i];
+    for(ll i = 1; i <= n; i++) cin >> bracket[i];
 
-    fo(i,1,n) cin in value[i];
-    fo(i,1,n) cin in bracket[i];
-
-    vector<vi> dp(n+5,vi(n+5));
+    vector<vector<ll>> dp(n+5,vector<ll>(n+5));
 
     // dp[i][j] = maximum sum possible of well bracketed sequence if we consider only from i to j ( both inclusive )
 
     // base case :
 
-    fo(i,1,n){
-        fo(j,1,i-1) {
+    for(ll i = 1; i <= n; i++){
+        for(ll j = 1; j <= i-1; j++) {
             dp[i][j] = -1;
         }
     }
-    fo(i,1,n) dp[i][i]= 0;
-    fo(i,1,n-1){
+    for(ll i = 1; i <= n; i++) dp[i][i]= 0;
+    for(ll i = 1; i <= n-1 ; i++){
         if(bracket[i+1]-bracket[i] == k){
             dp[i][i+1] = value[i] + value[i+1];
         }
@@ -51,7 +38,8 @@ int main() {
 
     // recurrence :
 
-    fo(diff,2,n-1){
+    for(ll diff = 2; diff <= n-1; diff++){
+        
         ll i = 1;
         ll j = 1+diff;
 
@@ -64,8 +52,8 @@ int main() {
                 dp[i][j] = max(dp[i][j-1],dp[i+1][j]);
             }
 
-            fo(m,i+1,j){
-                mmax(dp[i][j],dp[i][m-1]+dp[m][j]);
+            for(ll m = i+1; m <= j; m++){
+                dp[i][j] = max(dp[i][j],dp[i][m-1]+dp[m][j]);
             }
 
             i++;
@@ -75,7 +63,7 @@ int main() {
 
     // answer :
 
-    cout out dp[1][n];
+    cout << dp[1][n];
 
     return 0;
 }
