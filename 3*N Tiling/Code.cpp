c7b1d4357074d0fca8575086464fc44fc2086601
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2021-03-30 12:50:45 +0530

    Update Code.cpp

--- 3*N Tiling/Code.cpp
+++ 3*N Tiling/Code.cpp
@@ -1,56 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false); cin.tiille(NULL); cout.tie(NULL);
-typedef long long int ll;
-#define tc ll test;cin >> test;while(test--)
-#define vi vector<ll>
-#define pb push_back
-#define mp make_pair
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long int
 #define INF 0x3f3f3f3f3f
 #define MOD 1000000007
-#define ff first
-#define ss second
-#define in >>
-#define out <<
-#define space << " " <<
-#define spacef << " "
-#define fo(i,a,b) for(ll i = a; i <= b; i++)
-#define nextline out "\n"
-#define print(x) for(auto i : x ) cout out i spacef
-#define mmax(x,i) x = max(x,i)
-#define mmin(x,i) x = min(x,i)
 
 int main() {
 
-    tc{
+    fast;
+
+    ll test;
+    cin >> test;
+
+    while(test--) {
+
         ll k,n;
-        cin in k in n;
+        cin >> k >> n;
 
         if(k == 1){
-            if(n%3 == 0) cout out "1\n";
-            else cout out "0\n";
+            if(n%3 == 0) cout << "1\n";
+            else cout << "0\n";
         }
 
         if(k == 2){
-            vi dp(n+5);
+            vector<ll> dp(n+5);
 
             dp[1] = 0;
             dp[2] = 1;
             dp[3] = 1;
 
-            fo(i,4,n) {
+            for(ll i = 4; i <= n; i++){
                 dp[i] = dp[i-2] + dp[i-3];
                 dp[i] %= MOD;
             }
 
-            cout out dp[n]%MOD nextline;
+            cout << dp[n]%MOD << "\n";
         }
 
         if(k == 3){
 
-            vi f(n+5);
-            vi g(n+5);
-            vi h(n+5);
+            vector<ll> f(n+5);
+            vector<ll> g(n+5);
+            vector<ll> h(n+5);
 
             f[1] = 1;
             f[2] = 1;
@@ -64,7 +55,7 @@ int main() {
             h[2] = 1;
             h[3] = 1;
 
-            fo(i,4,n){
+            for(ll i = 4; i <= n; i++){
                 f[i] = f[i-1] + f[i-3] + 2*g[i-3];
                 g[i] = h[i-1]+g[i-3];
                 h[i] = f[i-2]+h[i-3];
@@ -74,7 +65,7 @@ int main() {
                 h[i] %= MOD;
             }
 
-            cout out f[n]%MOD nextline;
+            cout << f[n]%MOD << "\n";
         }
     }
 
